342efc67e23625dc55cf390d0ce5f62f2f5a46a4
gnshjoo <gnshjoo@gmail.com>
2021-03-05 11:29:34 +0900

    All you need to about State

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "kimchi",
-    image : "kakaka",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "ramen",
-    image : "hahaha",
-    rating: 7
-  },
-  {
-    id: 3,
-    name: "kimbab",
-    image : "hohoho",
-    rating: 1.0
-  },
-];
+class App extends React.Component{
+  state = {
+    count : 0
+  };
 
-function Food({name, picture, rating}) {
-  return (
-  <div>
-    <h2>I like {name}</h2>
-    <h4>rating 1s {rating}</h4>
-    <p>hello {picture}</p>
-  </div>
-)};
-
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(
-        dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-        )}
-    </div>
-  );
+  add = () => {
+    this.setState(current =>({count : current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current =>({count : current.count - 1}));
+  };
+  render() {
+    return (<div id="Potato">
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
